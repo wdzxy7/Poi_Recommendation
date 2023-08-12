2319b0f935741ac0f2dd8393781008c666a37f7f
wdzxy <1192936732@qq.com>
2023-08-12 13:33:01 +0800

    finish:    build_gcn_data.py: finish build_dist_graph_data function

--- build_gcn_data.py
+++ build_gcn_data.py
@@ -61,20 +61,46 @@ def build_user_graph_data():
         pickle.dump(data, open('./processed/{}/users/{}_user_graph_data.pkl'.format(data_type, i), 'wb'))
 
 
+def get_dist_dict(dist):
+    dist_dict = {}
+    pre_source = -1
+    for row in dist:
+        source = row[0]
+        dest = row[1]
+        dis = row[2]
+        if pre_source != source:
+            pre_source = source
+            dist_dict[int(source)] = {int(dest): dis}
+        else:
+            dist_dict[int(source)][int(dest)] = dis
+    return dist_dict
+
+
 def build_dist_graph_data():
-    global_graph = nx.read_gpickle('./graph/{}/global_graph.pkl'.format(data_type))
     graph_dist = np.loadtxt('./graph/{}/graph_dist.distlist'.format(data_type))
+    dist_dict = get_dist_dict(graph_dist)
     node_index = np.loadtxt('./graph/{}/graph_node_id2idx.txt'.format(data_type), dtype=int)
     node_dict = np_to_dict(node_index)
     global_edges = np.loadtxt('./graph/{}/graph_edge.edgelist'.format(data_type), dtype=int)
     edge_index = get_edge_index(global_edges)
-    node_feature = global_graph._node
     features = []
+    max_len = -1
     for key in node_dict.keys():
-        node_id = node_dict[key]
-        node = node_feature[node_id]
-        feature = [node['checkin_cnt'], node['poi_catid'], node['latitude'], node['longitude']]
-        features.append(feature)
+        try:
+            connect_nodes = dist_dict[key]
+            if len(connect_nodes) > max_len:
+                max_len = len(connect_nodes)
+        except:
+            features.append([])
+            continue
+        feature = []
+        for i in connect_nodes.keys():
+            feature.append(i)
+            feature.append(connect_nodes[i])
+        features.append(np.array(feature))
+    for i in range(len(features)):
+        pad_len = max_len * 2 - len(features[i])
+        features[i] = np.pad(features[i], (0, pad_len), 'constant')
     features = torch.LongTensor(features)
     data = Data(x=features, edge_index=edge_index)
     pickle.dump(data, open('./processed/{}/global_dist_data.pkl'.format(data_type), 'wb'))
@@ -84,3 +110,4 @@ if __name__ == '__main__':
     data_type = 'nyc'
     # build_global_graph_data()
     # build_user_graph_data()
+    build_dist_graph_data()
